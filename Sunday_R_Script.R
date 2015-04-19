54290c00339cf70e8b9e7733c596a7fe7fec54f6
lfuess <lefuess@gmail.com>
2015-04-19 14:19:28 -0500

    Cleaned up; install/load dplyr

--- Sunday_R_Script.R
+++ Sunday_R_Script.R
@@ -3,12 +3,14 @@
 # fuess@uta.edu
 # 4.18.2015
 
+# install dplyr
+# install.packages("dplyr",dependencies = TRUE)
+
+#load dplyr
+library("dplyr")
+
 #Read in data as tab-deliminated
-gap.in <- read.table("./output//combined_gapMinder.tsv", sep ="\t", header = TRUE)
+gap.in <- read.table("./output//combined_gapMinder.tsv", 
+                     sep ="\t", 
+                     header = TRUE)
 
-for (row.number in 1:10){for (col.number in 1:5) {print (gap.in[row.number,col.number])}}
-
-gap.in[1:10, 1:5]
-
-add.me <- function(x,y){x+y}
-add.me(3,4)
